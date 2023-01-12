f7789e8c7057a5b73db2f072535a2056a4b96af5
Ian França <93686060+IanFranco7@users.noreply.github.com>
2023-01-11 23:00:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -21,8 +21,6 @@ for(let contador = 0; contador < ListaDeTeclas.length; contador++) {
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;
 
-    /* essa function () é uma função anonima que foi criado só pra não dar erro, pq caso chamasse o tocaSom direto, seria como se ao ABRIR a pagina já carregaria o som antes mesmo de clicar em algo(interagir), ai por isso chamou a anonima pq vc tá CRIANDO, então ta suave so criou mas ainda nao chamou teoricamente. */
-
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
